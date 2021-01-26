82953815689a809640374f1ec98674c8e39fa3e2
michael azulay <michazulay97@gmail.com>
2021-01-26 16:36:11 +0200

    updated - michael

--- details.js
+++ details.js
@@ -1,3 +1,10 @@
+const heroes = [
+    {name: "hulk" , power:["smash"] , src: "הענק הירוק.jpg"};
+    {name: "batman" , power:["gudjets"] , src: ""};
+    {name: "superman" , power:["fly"] , src: ""};
+    {name: "captain-america" , power:"shield" , src: ""};
+];
+
 document.querySelector('#add').addEventListener('click',()=>{
     location.href = "form.html";
 });
