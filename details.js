56edf93fd28c2ab6a5dd6701fcd919f65f9d4d4d
Amit <ogamitctaffi@gmail.com>
2021-01-26 16:47:06 +0200

    updated

--- details.js
+++ details.js
@@ -1,11 +1,25 @@
-const heroes = [
-    {name: "hulk" , power:["smash"] , src: "הענק הירוק.jpg"};
-    {name: "batman" , power:["gudjets"] , src: ""};
-    {name: "superman" , power:["fly"] , src: ""};
-    {name: "captain-america" , power:"shield" , src: "https://d3m9l0v76dty0.cloudfront.net/system/photos/650767/large/8324ef4967211b85dba0617474e7122b.jpg"};
+const heroes = [{
+    name: "hulk",
+    power: "smash",
+    src: ""
+  },
+  {
+    name: "batman",
+    power: "gadgets",
+    src: ""
+  },
+  {
+    name: "superman",
+    power: "fly",
+    src: ""
+  },
+  {
+    name: "captain-america",
+    power: "shield",
+    src: ""
+  }
 ];
 
-document.querySelector('#add').addEventListener('click',()=>{
-    location.href = "form.html";
+document.querySelector('#add').addEventListener('click', () => {
+  location.href = "form.html";
 });
-
