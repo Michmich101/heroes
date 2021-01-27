dd5503be704e8bf7a9b349ee92189335610efd0f
Amit <ogamitctaffi@gmail.com>
2021-01-27 10:37:38 +0200

    updated-amit

--- details.js
+++ details.js
@@ -24,8 +24,3 @@
      src: ""
    }
  ];
-
- document.querySelector('#add').addEventListener('click', () => {
-   location.href = "form.html";
- });
- // >>>>>>> 56edf93fd28c2ab6a5dd6701fcd919f65f9d4d4d
