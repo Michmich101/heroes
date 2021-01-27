50cf38d88fe8838b36c9dca2005add594c7ad6ef
Amit <ogamitctaffi@gmail.com>
2021-01-27 10:32:31 +0200

    updated-amit

--- details.js
+++ details.js
@@ -1,35 +1,31 @@
-<<<<<<< HEAD
-document.querySelector('#add').addEventListener('click',()=>{
-        location.href = "form.html";
-});
+ // HEAD
+ document.querySelector('#add').addEventListener('click', () => {
+   location.href = "form.html";
+ });
 
+ const heroes = [{
+     name: "hulk",
+     power: "smash",
+     src: ""
+   },
+   {
+     name: "batman",
+     power: "gadgets",
+     src: ""
+   },
+   {
+     name: "superman",
+     power: "fly",
+     src: ""
+   },
+   {
+     name: "captain-america",
+     power: "shield",
+     src: ""
+   }
+ ];
 
-
-
-=======
-const heroes = [{
-    name: "hulk",
-    power: "smash",
-    src: ""
-  },
-  {
-    name: "batman",
-    power: "gadgets",
-    src: ""
-  },
-  {
-    name: "superman",
-    power: "fly",
-    src: ""
-  },
-  {
-    name: "captain-america",
-    power: "shield",
-    src: ""
-  }
-];
-
-document.querySelector('#add').addEventListener('click', () => {
-  location.href = "form.html";
-});
->>>>>>> 56edf93fd28c2ab6a5dd6701fcd919f65f9d4d4d
+ document.querySelector('#add').addEventListener('click', () => {
+   location.href = "form.html";
+ });
+ // >>>>>>> 56edf93fd28c2ab6a5dd6701fcd919f65f9d4d4d
