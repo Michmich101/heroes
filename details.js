7f80aaf84704405b02a4335e72b953a7dfd310d7
Amit <ogamitctaffi@gmail.com>
2021-01-27 16:50:10 +0200

    updated=amit

--- details.js
+++ details.js
@@ -3,42 +3,45 @@
    location.href = "form.html";
  });
 
-window.addEventListener('load', ()=>{
-  const cardGroup = document.getElementById('cardsContainer');
-  for (let hero of defaultHeroes){
-  const card = document.createElement('div');
-  card.classList.add('card');
-  card.innerHTML = `<img src="${hero.src}" class="card-img-top img" alt="hero">
+ window.addEventListener('load', () => {
+   const heroes = JSON.parse(localStorage.getItem('heroesArr'));
+   const cardGroup = document.getElementById('cardsContainer');
+   for (let hero of heroes) {
+     const card = document.createElement('div');
+     card.classList.add('card');
+     card.innerHTML = `<img src="${hero.src}" class="card-img-top img" alt="hero">
   <div class="card-body">
     <h5 class="card-title">${hero.name}</h5>
     <p class="card-text">${hero.power}</p>
     <p class="card-text"><a href="edit.html" class="text-muted">Edit hero details</a></p>
   </div>`;
-  cardGroup.appendChild(card);
-  }
-});
-
-
-
- const defaultHeroes = [{
-     name: "Hulk",
-     power: "Smash Force",
-     src: "הענק הירוק.jpg"
-   },
-   {
-     name: "Batman",
-     power: "Gadgets Master",
-     src: "https://upload.wikimedia.org/wikipedia/he/c/cf/Batman-JimLee.jpg"
-   },
-   {
-     name: "Superman",
-     power: "Super strength",
-     src: "https://upload.wikimedia.org/wikipedia/he/7/72/Superman.jpg"
-   },
-   {
-     name: "Captain-America",
-     power: "Retractable Shield",
-     src: "https://d3m9l0v76dty0.cloudfront.net/system/photos/650767/large/8324ef4967211b85dba0617474e7122b.jpg"
+     cardGroup.appendChild(card);
    }
+ });
 
- ];
+ function saveToStorage() {
+   const defaultHeroes = [{
+       name: "Hulk",
+       power: "Smash Force",
+       src: "הענק הירוק.jpg"
+     },
+     {
+       name: "Batman",
+       power: "Gadgets Master",
+       src: "https://upload.wikimedia.org/wikipedia/he/c/cf/Batman-JimLee.jpg"
+     },
+     {
+       name: "Superman",
+       power: "Super strength",
+       src: "https://upload.wikimedia.org/wikipedia/he/7/72/Superman.jpg"
+     },
+     {
+       name: "Captain-America",
+       power: "Retractable Shield",
+       src: "https://d3m9l0v76dty0.cloudfront.net/system/photos/650767/large/8324ef4967211b85dba0617474e7122b.jpg"
+     }
+   ];
+   localStorage.setItem('heroesArr', JSON.stringify(defaultHeroes));
+ }
+
+ saveToStorage();
--- form.js
+++ form.js
@@ -1,31 +1,30 @@
-const heroes = [
-    {name: "hulk" , power:["smash"] , src: "הענק הירוק.jpg"},
-    {name: "batman" , power:["gudjets"] , src: "https://upload.wikimedia.org/wikipedia/he/c/cf/Batman-JimLee.jpg"},
-    {name: "superman" , power:["fly"] , src: "https://upload.wikimedia.org/wikipedia/he/7/72/Superman.jpg"},
-    {name: "captain-america" , power:"shield" , src: "https://d3m9l0v76dty0.cloudfront.net/system/photos/650767/large/8324ef4967211b85dba0617474e7122b.jpg"},
-];
-
-document.querySelector('#addBtn').addEventListener('click',(e)=>{
-    e.preventDefault();
-    location.href = "details.html";
-    printHeros();
+document.querySelector('#addBtn').addEventListener('click', (e) => {
+  e.preventDefault();
+  location.href = "details.html";
+  printHeros();
 });
+const nameHero = document.getElementById('nameHero'),
+  heroStrength = document.getElementById('heroStrength'),
+  heroImgSrc = document.getElementById('heroImgSrc');
 
-function printHeros(){
-    let nameHero = document.getElementById('nameHero').value;
-    let heroStrength = document.getElementById('heroStrength').value;
-    let heroImgSrc = document.getElementById('heroImgSrc').value;
-    localStorage.setItem('nameHero', nameHero);    
-    localStorage.setItem('heroStrength', heroStrength);    
-    localStorage.setItem('heroImgSrc', heroImgSrc);    
-    
-    // const cardsContainer = document.getElementById('cardsContainer');
-    const div = document.createElement('div');
-    div.innerHTML = `<h5>${nameHero}</h5>
-    <p calss="col">${heroStrength}</p>
-    <p calss="col"><a href = "edit.html">${heroImgSrc}</a></p>`
-    localStorage.getItem('nameHero');
-    localStorage.getItem('heroStrength');
-    localStorage.getItem('heroImgSrc');
-    cardsContainer.appendChild(div);    
+function printHeros() {
+  const heroes = JSON.parse(localStorage.getItem('heroesArr'));
+  const hero = {
+    name: nameHero.value,
+    strength: heroStrength.value,
+    src: heroImgSrc.value
+  };
+  heroes.push(hero);
+  localStorage.setItem('heroesArr', heroes);
+
+
+  // // const cardsContainer = document.getElementById('cardsContainer');
+  // const div = document.createElement('div');
+  // div.innerHTML = `<h5>${nameHero}</h5>
+  //   <p calss="col">${heroStrength}</p>
+  //   <p calss="col"><a href = "edit.html">${heroImgSrc}</a></p>`
+  // localStorage.getItem('nameHero');
+  // localStorage.getItem('heroStrength');
+  // localStorage.getItem('heroImgSrc');
+  // cardsContainer.appendChild(div);
 }
